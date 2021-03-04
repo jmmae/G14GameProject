96528641eeb9533040c7e3623b46072e72bef8ed
Qasim-b <65030799+Qasim-b@users.noreply.github.com>
2021-03-04 19:16:20 +0000

    setup levelhandler file, WIP

---
+++ LevelHandler.py
@@ -0,0 +1,9 @@
+class Level Handler:
+    def __init__(self):
+        None
+        self.levelInstructions = #instruction screen
+        self.levelCongrats = # level complete screen
+        self.levelHolder = # array to store level objects??
+    def updateLevel(self):
+        #in interaction class, if player.starlevel == x: grab new level from array and load
+        None
